91cb7123a2c8a05b5af4e6438cb546a333432fcf
ethanshipman <124001912+ethanshipman@users.noreply.github.com>
2025-01-14 15:41:43 -0700

    uploaded completed file

--- static/js/logic.js
+++ static/js/logic.js
@@ -47,3 +47,54 @@ d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/2.5_month.geo
       `).addTo(map);
   });
 });
+  
+// Adding legend
+let legend = L.control({ position: "bottomright" });
+
+legend.onAdd = function() {
+  let div = L.DomUtil.create("div");
+  div.style.backgroundColor = "white";
+  div.style.padding = "10px";
+  div.style.borderRadius = "5px";
+  div.style.boxShadow = "0 0 15px rgba(0, 0, 0, 0.2)";
+  div.style.fontSize = "12px";
+  div.style.lineHeight = "18px";
+  
+  // title for the legend
+  let title = L.DomUtil.create("h4", "", div);
+  title.textContent = "Earthquake Depth (km)";
+  title.style.margin = "0 0 10px";
+  title.style.textAlign = "left";
+  
+  // Depth intervals
+  let depths = [-10, 10, 30, 50, 70, 90];
+  
+  // legend row for each depth interval
+  depths.forEach((depth, index) => {
+    // adding a container for the legend row
+    let row = L.DomUtil.create("div", "", div);
+    row.style.display = "flex";
+    row.style.alignItems = "center";
+    row.style.marginBottom = "5px";
+  
+      // color box
+    let colorBox = L.DomUtil.create("span", "", row);
+    colorBox.style.backgroundColor = markerColor(depth + 1);
+    colorBox.style.width = "18px";
+    colorBox.style.height = "18px";
+    colorBox.style.marginRight = "8px";
+    colorBox.style.border = "1px solid black";
+    colorBox.style.opacity = "0.8";
+  
+          
+    let label = L.DomUtil.create("span", "", row);
+    label.textContent =
+        index < depths.length - 1
+            ? `${depth}–${depths[index + 1]}`
+            : `${depth}+`;
+    label.style.color = "#555";
+  });
+  
+    return div;
+};
+legend.addTo(map);
